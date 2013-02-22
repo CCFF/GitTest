76c1c823138d7161e5d56e413c76548f26e7db8a
Casper <casper@lucrasoft.nl>
2013-02-22 12:43:21 +0100

    h

--- GitTestApp/GitTestApp/Form1.Designer.cs
+++ GitTestApp/GitTestApp/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 108);
+            this.label1.Location = new System.Drawing.Point(183, 78);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
